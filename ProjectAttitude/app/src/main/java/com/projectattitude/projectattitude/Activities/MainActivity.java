9f7fdaa1866dd8ef270a397d90c27e7c575dac99
bfleyshe <bfleyshe@ualberta.ca>
2017-02-25 11:46:40 -0700

    Revert "Revert "Created the project and classes""
    This reverts commit c7095d6526ba9f967902c3e43fd6498808ecc81b.

---
+++ ProjectAttitude/app/src/main/java/com/projectattitude/projectattitude/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.projectattitude.projectattitude.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.projectattitude.projectattitude.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ ProjectAttitude/app/src/main/java/com/projectattitude/projectattitude/Controllers/MapController.java
@@ -0,0 +1,8 @@
+package com.projectattitude.projectattitude.Controllers;
+
+/**
+ * Created by Chris on 2/24/2017.
+ */
+
+public class MapController {
+}
