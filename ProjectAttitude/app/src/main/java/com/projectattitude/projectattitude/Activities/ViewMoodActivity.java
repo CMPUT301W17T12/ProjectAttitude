4306e813aa179da510674eac92612c594ab7b339
Chris Saunders <cs3@ualberta.ca>
2017-03-08 16:49:04 -0700

    View mood is working, just need to finish the buttons. Created a color map and swapped everything over to it

--- ProjectAttitude/app/src/main/java/com/projectattitude/projectattitude/Activities/ViewMoodActivity.java
+++ ProjectAttitude/app/src/main/java/com/projectattitude/projectattitude/Activities/ViewMoodActivity.java
@@ -2,11 +2,16 @@ package com.projectattitude.projectattitude.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.projectattitude.projectattitude.Abstracts.MoodActivity;
+import com.projectattitude.projectattitude.Objects.ColorMap;
 import com.projectattitude.projectattitude.Objects.Mood;
 import com.projectattitude.projectattitude.R;
+
+import java.util.HashMap;
 
 
 public class ViewMoodActivity extends MoodActivity {
@@ -14,9 +19,11 @@ public class ViewMoodActivity extends MoodActivity {
     private TextView emotionState;
     private TextView date;
     private TextView trigger;
-    private TextView explanation;
     private TextView socialSituation;
+    private Button editButton;
+    private Button deleteButton;
 
+    RelativeLayout r1;
     Intent intent;
 
 
@@ -30,10 +37,25 @@ public class ViewMoodActivity extends MoodActivity {
         emotionState = (TextView) findViewById(R.id.EmotionStateView);
         date = (TextView) findViewById(R.id.DateView);
         trigger = (TextView) findViewById(R.id.TriggerView);
-        explanation = (TextView) findViewById(R.id.ExplanationView);
         socialSituation = (TextView) findViewById(R.id.SocialSituationView);
+        editButton = (Button) findViewById(R.id.EditButton);
+        deleteButton = (Button) findViewById(R.id.DeleteButton);
+        r1 = (RelativeLayout) findViewById(R.id.activity_view_mood);
+        emotionState.setText("");
+        date.setText("");
+        trigger.setText("");
+        socialSituation.setText("");
 
         //TODO Set texts from the mood
+        Mood mood = (Mood) getIntent().getSerializableExtra("mood");
+        emotionState.setText(mood.getEmotionState());
+        date.setText(mood.getMoodDate().toString());
+        trigger.setText(mood.getTrigger());
+        socialSituation.setText(mood.getSocialSituation());
+        //Handle colors
+        ColorMap<String, Integer> map = new ColorMap<>();
+        r1.setBackgroundColor((Integer) map.get(mood.getEmotionState()));
+
     }
 
     /**
