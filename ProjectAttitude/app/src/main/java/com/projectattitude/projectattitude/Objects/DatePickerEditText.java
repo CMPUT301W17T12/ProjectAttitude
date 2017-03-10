177dbde3dc3e13e674a40989304e6da118a054fa
Frederic Sauve-Hoover <rsauveho@ualberta.ca>
2017-03-09 19:37:09 -0700

    Fixed datePickerEditText returns date with h/m/s as well, when picking a day other than the current day, takes h/m/s from current time.

---
+++ ProjectAttitude/app/src/main/java/com/projectattitude/projectattitude/Objects/DatePickerEditText.java
@@ -0,0 +1,85 @@
+package com.projectattitude.projectattitude.Objects;
+
+/**
+ * Created by rfsh on 2017-03-07.
+ */
+
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
+/**
+ * Source
+ * http://stackoverflow.com/questions/14933330/datepicker-how-to-popup-datepicker-when-click-on-edittext
+ */
+
+public class DatePickerEditText implements View.OnClickListener, DatePickerDialog.OnDateSetListener {
+    EditText editText;
+    private int day;
+    private int month;
+    private int year;
+    private Context _context;
+
+    public DatePickerEditText(Context context, int editTextViewID)
+    {
+        Activity act = (Activity)context;
+        this.editText = (EditText)act.findViewById(editTextViewID);
+        this.editText.setOnClickListener(this);
+        this._context = context;
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        this.year = year;
+        this.month = monthOfYear;
+        this.day = dayOfMonth;
+        updateDisplay();
+    }
+    public void setDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+        updateDisplay();
+    }
+    @Override
+    public void onClick(View v) {
+        Calendar calendar = Calendar.getInstance(TimeZone.getDefault());
+
+        DatePickerDialog dialog = new DatePickerDialog(_context, this,
+                calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH));
+
+        dialog.show();
+
+    }
+
+    public Date getDate(){
+        String date_str = editText.getText().toString();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.CANADA);
+        try {
+            Date date = sdf.parse(date_str);
+            Date currTime = new Date(System.currentTimeMillis());
+            return new Date(date.getYear(), date.getMonth(), date.getDay(),
+                    currTime.getHours(), currTime.getMinutes(), currTime.getSeconds());
+        } catch (ParseException e) {
+            return new Date(System.currentTimeMillis());
+        }
+    }
+
+    // updates the date in the birth date EditText
+    private void updateDisplay() {
+        editText.setText(new StringBuilder()
+                // Month is 0 based so add 1
+                .append(year).append("-").append(month + 1).append("-").append(day));
+    }
+}
