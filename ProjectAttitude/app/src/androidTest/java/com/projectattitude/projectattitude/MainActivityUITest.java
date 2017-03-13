3b1e9ab11ef1b4b4b0d4f324db72b610521c0a06
Chris Saunders <cs3@ualberta.ca>
2017-03-13 13:32:26 -0600

    Login and add/delete tests work

--- ProjectAttitude/app/src/androidTest/java/com/projectattitude/projectattitude/MainActivityUITest.java
+++ ProjectAttitude/app/src/androidTest/java/com/projectattitude/projectattitude/MainActivityUITest.java
@@ -2,11 +2,13 @@ package com.projectattitude.projectattitude;
 
 import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
 
-import com.projectattitude.projectattitude.Activities.CreateMoodActivity;
 import com.projectattitude.projectattitude.Activities.EditMoodActivity;
+import com.projectattitude.projectattitude.Activities.LoginActivity;
 import com.projectattitude.projectattitude.Activities.MainActivity;
 import com.robotium.solo.Solo;
+import com.projectattitude.projectattitude.Activities.CreateMoodActivity;
 
 /**
  * Created by Boris on 12/03/2017.
@@ -14,12 +16,12 @@ import com.robotium.solo.Solo;
  * This class starts in the login screen, logs in and attempts to make a mood.
  */
 
-public class MainActivityUITest extends ActivityInstrumentationTestCase2<MainActivity> {
+public class MainActivityUITest extends ActivityInstrumentationTestCase2<LoginActivity> {
 
     private Solo solo;
 
     public MainActivityUITest() {
-        super(com.projectattitude.projectattitude.Activities.MainActivity.class);
+        super(com.projectattitude.projectattitude.Activities.LoginActivity.class);
     }
 
     public void setUp() throws Exception{
@@ -30,31 +32,32 @@ public class MainActivityUITest extends ActivityInstrumentationTestCase2<MainAct
         Activity activity = getActivity();
     }
 
-    private void testCreateMood(){
-        LoginActivityUITest loginActivityUITest = new com.projectattitude.projectattitude.LoginActivityUITest();    //login
-        loginActivityUITest.testLogin();
+
+    public void testCreateMood(){
+        solo.enterText((EditText)solo.getView(R.id.usernameField), "tester");
+        solo.clickOnView(solo.getView(R.id.signInButton));
         solo.assertCurrentActivity("Wrong activity", MainActivity.class);
         solo.clickOnView(solo.getView(R.id.addMoodButton));
         solo.assertCurrentActivity("Wrong activity", CreateMoodActivity.class);
 
         assertTrue(solo.searchText("Select an emotional state"));   //creating mood
         solo.clickOnText("Select an emotional state");
-        assertTrue(solo.searchText("Happy"));
-        solo.clickOnText("Happy");
+        assertTrue(solo.searchText("Happiness"));
+        solo.clickOnText("Happiness");
 
         assertTrue(solo.searchButton("Save"));  //saving mood
         solo.clickOnButton("Save");
 
         solo.assertCurrentActivity("Wrong activity", MainActivity.class);
 
-        assertTrue(solo.searchText("Happy"));   //checking if mood exist
+        assertTrue(solo.searchText("Happiness"));   //checking if mood exist
 
         deleteFirstMood();
 
-        assertFalse(solo.searchText("Happy"));
+        assertFalse(solo.searchText("Happiness"));
     }
 
-    private void testEditMood(){
+    public void testEditMood(){
         LoginActivityUITest loginActivityUITest = new com.projectattitude.projectattitude.LoginActivityUITest();    //login
         loginActivityUITest.testLogin();
         solo.assertCurrentActivity("Wrong activity", MainActivity.class);
@@ -93,10 +96,10 @@ public class MainActivityUITest extends ActivityInstrumentationTestCase2<MainAct
         assertFalse(solo.searchButton("Happy"));
     }
 
-    private void deleteFirstMood(){ //deleting mood
+    public void deleteFirstMood(){ //deleting mood
         solo.clickLongInList(0);
         assertTrue(solo.searchText("Delete"));
-        solo.clickOnButton("Delete");
+        solo.clickOnText("Delete");
     }
 
     @Override
