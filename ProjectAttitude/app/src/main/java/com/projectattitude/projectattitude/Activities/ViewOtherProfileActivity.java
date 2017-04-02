2c0f776cbc79f32f9be6b0f5cc2bebfcb63641b6
TheBurnerGuy <henryweilo@hotmail.com>
2017-04-02 16:25:45 -0600

    new changes

--- ProjectAttitude/app/src/main/java/com/projectattitude/projectattitude/Activities/ViewOtherProfileActivity.java
+++ ProjectAttitude/app/src/main/java/com/projectattitude/projectattitude/Activities/ViewOtherProfileActivity.java
@@ -52,7 +52,7 @@ public class ViewOtherProfileActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_profile);
+        setContentView(R.layout.activity_view_other_profile);
 
         image = (ImageView) findViewById(R.id.profileImage);
         nameView = (TextView) findViewById(R.id.profileUname);
